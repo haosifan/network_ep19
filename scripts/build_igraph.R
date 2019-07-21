9efaec690feb15830df917c0cb5cccc4b0b86134
Stefan Haußner <stefan.haussner@uni-due.de>
2019-07-21 22:33:48 +0200

    second round of data retrieval
    some cases were not covered
    sometimes this was due to the fact that the account had no friends, but not always

---
+++ scripts/build_igraph.R
@@ -0,0 +1,17 @@
+library(igraph)
+library(tidyverse)
+
+
+edge_list <- read_csv2("data/edge_list.csv")
+d_full_information <- read_csv2("data/meps_full_list.csv")
+d_mep_twitter <- read_csv2("data/meps_twitter.csv")
+
+d_mep_twitter %>% filter(!(twitter_id %in% edge_list$user))
+
+net_igraph <- igraph::graph_from_data_frame(d = edge_list, directed = TRUE)
+
+net_igraph <- set_vertex_attr(graph = net_igraph, name = "fullName", value = d_full_information$name)
+net_igraph <- set_vertex_attr(graph = net_igraph, name = "GroupLong", value = d_full_information$group)
+net_igraph <- set_vertex_attr(graph = net_igraph, name = "country", value = d_full_information$country)
+net_igraph <- set_vertex_attr(graph = net_igraph, name = "nationalParty", value = d_full_information$natparty)
+
